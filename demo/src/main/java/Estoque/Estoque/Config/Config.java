0f16fda0fe0696ff118522e23beb00c58a66388c
Paulo Henrique Ferreira dos Santos <paulohenriquebq2011@hotmail.com>
2025-05-20 08:46:33 -0300

    test

---
+++ demo/src/main/java/Estoque/Estoque/Config/Config.java
@@ -0,0 +1,27 @@
+package Estoque.Estoque.Config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica CORS para todos os endpoints
+                        .allowedOrigins("http://localhost:3000") // Frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
+
